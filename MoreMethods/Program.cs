bd3a9f37ff872964774a7a0576789e7f85f97d0a
Zachary Haggerty <zhaggerty24@gmail.com>
2022-10-05 03:12:51 -0400

    initial commit

---
+++ MoreMethods/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace MoreMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Name();
+            Age();
+            Multiply();
+        }
+        static void Name()
+        {
+            string myName = "Zach";
+
+            Console.WriteLine($" Hello my name is {myName}");
+        }
+        static void Age()
+        {
+            string myAge = "27";
+
+            Console.WriteLine($" Nice to meet you I am {myAge} years old");
+        }
+        static void Multiply()
+        {
+            Console.WriteLine(2 *(4 + 6));
+           
+        }
+    }
+}
